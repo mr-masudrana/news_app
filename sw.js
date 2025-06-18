8aec18b6cbebb77d9156d90a3571b36625bfbef7
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2025-06-19 01:19:00 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'bd-news-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/rss.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
